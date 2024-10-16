193c3a7bf49b2186f340db245eb32ad22dfacfec
Elijah925 <472880555@qq.com>
2024-10-16 23:28:00 +1100

    merge 3 question to 1 file

--- 2024-10-16--41-238-560.java
+++ 2024-10-16--41-238-560.java
@@ -1,9 +1,50 @@
+//第41题
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        // 排序数组
+        Arrays.sort(nums);
+        int minPos = Integer.MAX_VALUE;
+        // 遍历寻找数组中的最小正整数
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] > 0 && minPos > nums[i]) {
+                minPos = nums[i];
+            }
+        }
+        // 判断minPos是否为1 , 优化方案中初始化为1就不用再次判断
+        if (minPos == 1) {
+            // 如果最小正数为1, 开始minPos+1
+            for (int i : nums) {
+                //如果右重复的就+1
+                if (i == minPos) {
+                    minPos++;
+                }
+            }
+        } else {
+            return 1;
+        }
+        return minPos;
+
+        //优化方案 整合到1个for循环中
+        // int minPos = 1;
+        // for (int i = 0; i < nums.length; i++) {
+        //     if (nums[i] > 0 && nums[i] == minPos) {
+        //         minPos++;
+        //     } else if (nums[i] > minPos) {
+        //         return minPos;
+        //     }
+        // }
+        // return minPos;
+    }
+}
+
+
+//第238题
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        //暴力破解 时间复杂度 O(n平方) 
+        //暴力破解 时间复杂度 O(n平方)
         // int[] result = new int[nums.length];
         // for(int i=0;i<nums.length;i++){
-        //     int sum=1;       
+        //     int sum=1;
         //     for(int j=0;j<nums.length;j++){
         //         if(i!=j){
         //             sum *= nums[j];
@@ -50,5 +91,33 @@ class Solution {
         }
 
         return result;
+    }
+}
+
+//第560题
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int sum=0;
+        int count=0;
+        //创建哈希表,第一个Integer是前缀和,第二个integer是出现次数
+        HashMap<Integer,Integer> arr_sum = new HashMap();
+        arr_sum.put(0,1);
+
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            if(arr_sum.containsKey(sum-k))
+            {
+                count+= arr_sum.get(sum-k);
+            }
+            if(arr_sum.containsKey(sum))
+            {
+                arr_sum.put(sum,arr_sum.get(sum)+1);
+            }
+            else
+            {
+                arr_sum.put(sum,1);
+            }
+        }
+        return count;
     }
 }
