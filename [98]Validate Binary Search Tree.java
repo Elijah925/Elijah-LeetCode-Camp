25f3d82b6a15ac0a01f543590905cf87a4a7a93d
Elijah925 <472880555@qq.com>
2024-10-27 19:15:14 +1100

    2024-10-27 (update 98 comment)

--- [98]Validate Binary Search Tree.java
+++ [98]Validate Binary Search Tree.java
@@ -15,8 +15,12 @@
  */
 class Solution {
     TreeNode prev;
-
+    int count =0;
     public boolean isValidBST(TreeNode root) {
+        //解题的核心逻辑用递归方法, 从左到右,从下到上进行判断,
+        //prev总是在当前节点的左边,所以判断条件为prev>root
+        //即左边的节点一点要小于右边节点
+
         //判断边界, root为空返回ture, case 3
         if (root == null){return true;}
 
@@ -31,13 +35,21 @@ class Solution {
         if (prev != null && prev.val >= root.val)
         {return false;}
 
+        if (prev != null){
+            System.out.println("prev= "+prev.val+", root= "+root.val);
+        }
+
         //将当前节点保存为"前节点"
         //将5保存到前节点中
         prev = root;
-
+        count++;
+        System.out.println(count);
         //迭代判断右树
-        //第二次迭代中, 当前节点为5,prev为5, 右树节点4进入迭代
-        //此时当前节点为4,prev为5, 5>=4, 判断失败返回false
+        //第二次迭代中, 当前节点为5, prev为5, 右树节点进入递归
+        //进入递归后此时当前节点为3, prev为5, 5>=3, 判断失败返回false
+        //逻辑为进入遍历右子树时,从右子树的最左最下叶节点开始判断
+        //case 4中, 3<5, 然后5<6, 然后6<7
+        //第四次递归prev为7, 进入7的右节点4,失败返回
         if (!isValidBST(root.right))
         {return false;}
 
